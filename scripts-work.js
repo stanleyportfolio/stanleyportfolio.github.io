368505178136fb0b32aa1ae00d6875f86c173dc5
stanvet <stanvet>
2024-11-14 23:48:48 +0800

    latest update 2024

---
+++ scripts-work.js
@@ -0,0 +1,25 @@
+
+const itemWrappers = document.querySelectorAll('.item-wrapper');
+document.querySelector('body').addEventListener('click', function(e){
+    let el = e.target;
+    if (el.dataset.page != 1) return;
+    el.dataset.page = parseInt(el.dataset.page) + 1; 
+    
+    itemWrappers.forEach(item=>{item.classList.remove('selected')})
+});
+
+document.querySelector('#list-close').addEventListener('click', function(e){
+    itemWrappers.forEach(item=>{item.classList.remove('selected')})
+});
+
+document.querySelector('#list-lightbox').addEventListener('click', function(e){
+    itemWrappers.forEach(item=>{item.classList.remove('selected')})
+});
+
+itemWrappers.forEach(item => {
+    item.addEventListener('click', () => {
+        console.log(1);
+        itemWrappers.forEach(item=>{item.classList.remove('selected')})
+        item.classList.add('selected');
+    });
+});
