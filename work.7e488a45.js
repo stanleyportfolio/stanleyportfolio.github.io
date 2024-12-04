6504ea00bd4bb1322dfc9bcf09238938b0a914b5
stanvet <stanvet>
2024-12-05 01:24:27 +0800

    Updates

--- work.7e488a45.js
+++ work.7e488a45.js
@@ -1,3 +1,8 @@
+const switchClasses = [
+    "backend-noswitch",
+    "backend-switch",
+    "backend-switch-2"
+];
 const itemWrappers = document.querySelectorAll(".item-wrapper");
 document.querySelector("body").addEventListener("click", function(e) {
     let el = e.target;
@@ -13,8 +18,24 @@ document.querySelector("#list-close").addEventListener("click", function(e) {
     });
 });
 document.querySelector("#list-switch").addEventListener("click", function(e) {
-    document.body.classList.toggle("backend-switch");
+    toggleClass(document.body, switchClasses);
 });
+document.querySelector("#switch1").addEventListener("click", function(e) {
+    toggleClass(document.body, switchClasses, switchClasses[0]);
+});
+document.querySelector("#switch2").addEventListener("click", function(e) {
+    toggleClass(document.body, switchClasses, switchClasses[1]);
+});
+document.querySelector("#switch3").addEventListener("click", function(e) {
+    toggleClass(document.body, switchClasses, switchClasses[2]);
+});
+function toggleClass(targetElement, classes, specificClass = false) {
+    if (!targetElement || !classes) return;
+    const currentClass = classes.find((c)=>targetElement.classList.contains(c));
+    targetElement.classList.remove(currentClass);
+    if (specificClass) targetElement.classList.add(specificClass);
+    else targetElement.classList.add(classes[(classes.indexOf(currentClass) + 1) % classes.length]);
+}
 document.querySelector("#list-lightbox").addEventListener("click", function(e) {
     itemWrappers.forEach((item)=>{
         item.classList.remove("selected");
